239f9ca3b0d114a965596b4acbec4740bd60a12a
mhanshory <hakim09mochammad@gmail.com>
2024-08-10 07:24:18 +0700

    Latihan CREATE GIT innit

---
+++ app.js
@@ -0,0 +1,26 @@
+const form = document.querySelector('#search-form');
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+
+    document.querySelectorAll('img').forEach((img) => img.remove());
+
+    const keyword = form.elements.query.value;
+    const config = {
+        params: { q: keyword},
+    };
+    const res = await axios.get('https://api.tvmaze.com/search/shows', config);
+    getImages(res.data);
+    form.elements.query.value; " ";
+});
+
+const getImages = (show) => {
+    for (let result of show) {
+        if (result.show.image) {
+            const img = document.createElement('img');
+            img.src = result.show.image.medium;
+            document.body.append(img);
+        }
+    }
+}
